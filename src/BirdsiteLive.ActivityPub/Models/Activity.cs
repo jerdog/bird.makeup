07a9daf735d7b63e53e1d62bdf98311ef7d5c132
jofkos <jofkos@git.sr.ht>
2023-12-08 17:19:26 +0100

    Ignoring Activity @context during deserialization, as it may be a JSON object instead of just a string (Firefish)

--- src/BirdsiteLive.ActivityPub/Models/Activity.cs
+++ src/BirdsiteLive.ActivityPub/Models/Activity.cs
@@ -4,11 +4,15 @@ namespace BirdsiteLive.ActivityPub
 {
     public class Activity
     {
-        [JsonPropertyName("@context")]
+        [JsonIgnore]
         public string context { get; set; }
         public string id { get; set; }
         public string type { get; set; }
         public string actor { get; set; }
+
+        // Avoids deserialization of @context, as it may be a JSON object instead of a string (e.g., on Firefish)
+        [JsonPropertyName("@context")]
+        public string SerializedContext => context;
 
         //[JsonPropertyName("object")]
         //public string apObject { get; set; }

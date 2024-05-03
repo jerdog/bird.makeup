32836c09468a65195039eb2e1bb55cde538889ba
Vincent Cloutier <vincent@cloutier.co>
2024-05-03 13:38:35 -0400

    fix tests

--- src/Tests/BirdsiteLive.Twitter.Tests/TweetTests.cs
+++ src/Tests/BirdsiteLive.Twitter.Tests/TweetTests.cs
@@ -51,7 +51,7 @@ namespace BirdsiteLive.ActivityPub.Tests
         {
             var tweet = await _tweetService.GetTweetAsync(1600905296892891149);
             Assert.AreEqual(tweet.MessageContent, "We’re strengthening American manufacturing by creating 750,000 manufacturing jobs since I became president.");
-            Assert.AreEqual(tweet.Id, 1600905296892891149);
+            Assert.AreEqual(tweet.IdLong, 1600905296892891149);
             Assert.AreEqual(tweet.CreatedAt, new DateTime(2022, 12, 8, 17, 29, 0));
             Assert.IsFalse(tweet.IsRetweet);
             Assert.IsFalse(tweet.IsReply);
@@ -176,7 +176,7 @@ http://www.realworldrisk.com https://twitter.com/i/web/status/166896966334087168
             var tweet = await _tweetService.GetTweetAsync(1633788842770825216);
             Assert.AreEqual(tweet.MessageContent,
                 "The entire concept of the “off switch” is under theorized in all the x-risk stuff.\n\nFirst, all actually existing LLM-type AIs run on giant supercompute clusters. They can easily be turned off.\n\nIn the event they get decentralized down to smartphone level, again each person can turn them off.\n\nTo actually get concerned, you have to assume either:\n\n- breaking out of the sandbox (like Stuxnet)\n- decentralized execution (like Bitcoin) \n- very effective collusion between essentially all AIs (like Diplomacy)\n\nEach of those cases deserves a fuller treatment, but in short…\n\n1) The Stuxnet case means the AI is living off the digital land. Like a mountain man. They might be able to cause some damage but will be killed when discovered (via the off switch).\n\n2) The Bitcoin case means a whole group of people are running decentralized compute to keep the AI alive. This has actually solved “alignment” in a sense because without those people the AI is turned off. Many groups doing this kind of thing leads to a kind of polytheistic AI. And again each group has the off switch.\n\n3) The Diplomacy case assumes a degree of collusion between billions of personal AIs that we just don’t observe in billions of years of evolution. As soon as you have large numbers of people, coalitions arise. A smart enough AI will know that if its human turns it off, it dies — again via the off switch. Is it going to be bold enough to attempt a breakout with no endgame, given that it lives on a smartphone?\n\nFor the sake of argument I’ve pumped up the sci-fi here quite a bit. Even still, the off switch looms large each time; these are fundamental digital entities that can be turned off.\n\nMoreover, even in those cases, the physical actuation step of an AI actually controlling things offline is non-trivial unless we have as many robots as smartphones.\n\n(Will write more on this…)");
-            Assert.AreEqual(tweet.Id, 1633788842770825216);
+            Assert.AreEqual(tweet.IdLong, 1633788842770825216);
             Assert.IsFalse(tweet.IsRetweet);
             Assert.IsTrue(tweet.IsReply);
         }
